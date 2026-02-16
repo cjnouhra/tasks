8230b4897fb6262f9c39531ce214c2450e4e2d69
Cameron Nouhra <cnouhra@udel.edu>
2026-02-15 21:37:56 -0500

    task 2 decorated the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import chuzz from "./mrchuzz.jpg";
 
 function App(): React.JSX.Element {
     return (
@@ -7,8 +9,45 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <header>Cameron Nouhra</header>
-            <header>Hello World</header>
+            <h1>Cameron Nouhra Hello World</h1>
+
+            <img src={chuzz} style={{ width: "150px" }} alt="my friend's cat" />
+            <ol>
+                <li>Cherries</li>
+                <li>Oranges</li>
+                <li>Watermelon</li>
+            </ol>
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        Column 1
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        Column 2
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
