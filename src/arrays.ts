20577993c0b67e9db24c973d6e4ce93128c7ac1d
Cameron Nouhra <cnouhra@udel.edu>
2026-02-22 20:29:49 -0500

    countShortWords

--- src/arrays.ts
+++ src/arrays.ts
@@ -68,7 +68,10 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    let shortened: string[] = words.filter(
+        (word: string): boolean => word.length < 4,
+    );
+    return shortened.length;
 }
 
 /**
