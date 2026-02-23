27aa6ab13a664b5d4596c12352c41f8e42f47aad
Cameron Nouhra <cnouhra@udel.edu>
2026-02-22 20:13:54 -0500

    strings to integers

--- src/arrays.ts
+++ src/arrays.ts
@@ -28,7 +28,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    let ints: number[] = numbers.map(
+        (num: string): number => parseInt(num) || 0,
+    );
+    return ints;
 }
 
 /**
