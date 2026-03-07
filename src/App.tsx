5c98b84a841243dcd1020b2ecb09a78a58685bb7
Cameron Nouhra <cnouhra@udel.edu>
2026-03-07 12:34:19 -0500

    Reveal Answer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Button, Col, Container, Row } from "react-bootstrap";
-import chuzz from "./mrchuzz.jpg";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
@@ -15,49 +13,6 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <h1>Cameron Nouhra Hello World</h1>
-
-            <img src={chuzz} style={{ width: "150px" }} alt="my friend's cat" />
-            <ol>
-                <li>Cherries</li>
-                <li>Oranges</li>
-                <li>Watermelon</li>
-            </ol>
-            <Button
-                onClick={() => {
-                    console.log("Hello World!");
-                }}
-            >
-                Log Hello World
-            </Button>
-            <Container>
-                <Row>
-                    <Col>
-                        Column 1
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                backgroundColor: "red",
-                            }}
-                        ></div>
-                    </Col>
-                    <Col>
-                        Column 2
-                        <div
-                            style={{
-                                width: "100px",
-                                height: "100px",
-                                backgroundColor: "red",
-                            }}
-                        ></div>
-                    </Col>
-                </Row>
-            </Container>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
             <hr></hr>
             <Counter></Counter>
             <hr />
